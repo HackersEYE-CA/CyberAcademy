7e283ac0dafe891de0ee87a29ce5985767a0ae8e
HackersEYE-CA <67216442+HackersEYE-CA@users.noreply.github.com>
2020-07-02 12:08:21 +0300

    Add files via upload

---
+++ scripts/ovpn_change_ip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PUBLICIP=$(curl -s ifconfig.me)
+sudo sqlite3 /usr/local/openvpn_as/etc/db/config_local.db "update config set value='$PUBLICIP' where name='host.name';"
+sudo systemctl restart openvpnas
